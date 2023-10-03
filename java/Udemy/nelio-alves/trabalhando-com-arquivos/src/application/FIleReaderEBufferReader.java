8560dcb44ce3aed1f8fa9e51604aecf4f6b25a11
aldopereira.gyn@gmail.com <aldopereira.gyn@gmail.com>
2023-10-03 12:05:06 -0300

    Nelio Alves: trabalhando com arquivos: Bloco try-with-resources

--- java/Udemy/nelio-alves/trabalhando-com-arquivos/src/application/FIleReaderEBufferReader.java
+++ java/Udemy/nelio-alves/trabalhando-com-arquivos/src/application/FIleReaderEBufferReader.java
@@ -7,30 +7,40 @@ import java.io.IOException;
 public class FIleReaderEBufferReader {
 
     public static void main(String[] args) {
-        String path = "/home/aldo/texto.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
+//        FileReader fr = null;
+//        BufferedReader br = null;
+//        try {
+//            fr = new FileReader(path);
+//            br = new BufferedReader(fr);
+//
+//            String line =  br.readLine();
+//
+//            while (line != null){
+//                System.out.println(line);
+//                line = br.readLine();
+//            }
+//        }catch (IOException e){
+//            System.out.println("Error "+e.getMessage());
+//        }finally {
+//            try {
+//                if(br != null ) br.close();
+//                if(fr != null) fr.close();
+//            }
+//            catch (IOException e){
+//                e.printStackTrace();
+//            }
+//        }
 
-            String line =  br.readLine();
-
+    String path = "/home/aldo/teste.html";
+        try(BufferedReader br = new BufferedReader(new FileReader(path))){
+            String line = br.readLine();
             while (line != null){
                 System.out.println(line);
                 line = br.readLine();
             }
         }catch (IOException e){
-            System.out.println("Error "+e.getMessage());
-        }finally {
-            try {
-                if(br != null ) br.close();
-                if(fr != null) fr.close();
-            }
-            catch (IOException e){
-                e.printStackTrace();
-            }
+            System.out.println("Error: "+e.getMessage());
         }
     }
 }
