40cae2a46badef4176baaf333f026a049ed3d5c8
Aldo <aldopereira.gyn@gmail.com>
2023-10-09 16:33:04 -0300

    Curso Nelio Alves: Web Services com spring boot JPA/Hibernat: OrderStatus enum

--- java/Udemy/nelio-alves/curso-spring-boot/src/main/java/curso/entities/Order.java
+++ java/Udemy/nelio-alves/curso-spring-boot/src/main/java/curso/entities/Order.java
@@ -1,7 +1,7 @@
 package curso.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import curso.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -23,6 +23,18 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone ="GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -31,9 +43,10 @@ public class Order implements Serializable {
 
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
