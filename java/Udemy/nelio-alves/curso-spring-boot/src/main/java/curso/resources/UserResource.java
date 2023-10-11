52ead49d6b94b0bfda982bb36863d00042a19a7d
Aldo <aldopereira.gyn@gmail.com>
2023-10-11 11:00:53 -0300

    Curso Nelio Alves: Web Services com spring boot JPA/Hibernate: User delete

--- java/Udemy/nelio-alves/curso-spring-boot/src/main/java/curso/resources/UserResource.java
+++ java/Udemy/nelio-alves/curso-spring-boot/src/main/java/curso/resources/UserResource.java
@@ -38,4 +38,10 @@ public class UserResource {
                 .toUri();
         return  ResponseEntity.created(uri).body(obj);
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
