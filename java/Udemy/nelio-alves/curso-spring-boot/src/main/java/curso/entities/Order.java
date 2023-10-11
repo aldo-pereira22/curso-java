2b75bae453c5e1f9eafb609b57a92e0d471a71bc
Aldo <aldopereira.gyn@gmail.com>
2023-10-11 10:25:22 -0300

    Curso Nelio Alves: Web Services com spring boot JPA/Hibernate: Payment one-to-one association

--- java/Udemy/nelio-alves/curso-spring-boot/src/main/java/curso/entities/Order.java
+++ java/Udemy/nelio-alves/curso-spring-boot/src/main/java/curso/entities/Order.java
@@ -27,6 +27,9 @@ public class Order implements Serializable {
 
     private Integer orderStatus;
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public OrderStatus getOrderStatus() {
         return OrderStatus.valueOf(orderStatus);
     }
@@ -77,6 +80,14 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     public Set<OrderItem> getItems(){
