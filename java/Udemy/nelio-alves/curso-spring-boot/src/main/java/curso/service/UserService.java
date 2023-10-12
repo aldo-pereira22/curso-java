723cf7acc00811dc430da0216c0a3c8f9aca6f15
Aldo <aldopereira.gyn@gmail.com>
2023-10-12 09:50:47 -0300

    Curso Nelio Alves: Web Services com spring boot JPA/Hibernate: Exception Handling - findById

--- java/Udemy/nelio-alves/curso-spring-boot/src/main/java/curso/service/UserService.java
+++ java/Udemy/nelio-alves/curso-spring-boot/src/main/java/curso/service/UserService.java
@@ -2,6 +2,7 @@ package curso.service;
 
 import curso.entities.User;
 import curso.repositories.UserRepository;
+import curso.service.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow( () -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
